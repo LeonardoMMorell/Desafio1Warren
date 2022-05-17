a7a287cb7b9aca8ee21da952a3fd432fa08531fd
LeonardoMMorell <leonardo.morell@warren.com.br>
2022-05-17 16:29:42 -0300

    Updates

--- DesafioWarren.API/Services/ICustomerServices.cs
+++ DesafioWarren.API/Services/ICustomerServices.cs
@@ -4,7 +4,6 @@ namespace DesafioWarren.API.Data
 {
     public interface ICustomerServices
     {
-        public List<Customer> CustomersClients { get; set; }
         public void Add(Customer customer);
         public void Update(Customer customer, Customer CustomerUpdated);
         public void DeleteCustomer(Customer customer);
@@ -12,6 +11,7 @@ namespace DesafioWarren.API.Data
         List<Customer> SearchFullName(string FullName);
         List<Customer> SearchEmail(string Email);
         List<Customer> SearchCpf(string Cpf);
-        List<Customer> GetAll(Predicate<Customer> predicate);
+        List<Customer> GetAll(Predicate<Customer> predicate = null);
+        Customer GetSingle(Func<Customer, bool> predicate);
     }
 }
