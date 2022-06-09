9108dba98c58e326f25f981379d99f472ff6bf08
LeonardoMMorell <103209936+LeonardoMMorell@users.noreply.github.com>
2022-06-09 16:58:10 -0300

    Update AppServices/Dtos/UpdateCustomerRequest.cs
    Co-authored-by: Vinícius de Oliveira <vinicius.oliveira@warren.com.br>

--- AppServices/Dtos/UpdateCustomerRequest.cs
+++ AppServices/Dtos/UpdateCustomerRequest.cs
@@ -6,7 +6,6 @@ namespace AppServices.Dtos
     {
         public string FullName { get; set; }
         public string Email { get; set; }
-        public string EmailConfirmation { get; set; }
         public string Cpf { get; set; }
         public string Cellphone { get; set; }
         public DateTime Birthdate { get; set; }
